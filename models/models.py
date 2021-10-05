4c5af6abb281833cfafbe8335b733bf82ca474d5
murtaja1 <murtajaadnan7@gmail.com>
2021-10-05 11:21:03 +0000

    complete payment_report

--- models/models.py
+++ models/models.py
@@ -21,4 +21,33 @@ class AccountMove(models.Model):
 
 class AccountPayment(models.Model):
     _inherit = "account.payment"
-    pay_type = fields.Selection([('cash', 'نقد'), ('check', 'صك')])
+    recieve_type = fields.Selection(
+        [('cash', 'نقد'), ('check', 'صك')], string="نوع السداد")
+
+    def get_latest_rec(self, records):
+        for rec in records:
+            tep = rec
+            if tep.batch_id.start_date < rec.batch_id.start_date:
+                tep = rec
+
+        return tep
+
+
+class Student(models.Model):
+    _inherit = "res.partner"
+
+    student_id = fields.Many2one(
+        "op.student", string="Related Student", compute="_compute_student_id")
+
+    def _compute_student_id(self):
+        for record in self:
+            student_id = self.env['op.student'].search(
+                [('partner_id', '=', record.id)])
+            record.student_id = student_id.id
+
+
+class Stage(models.Model):
+    _inherit = 'op.student'
+
+    stage = fields.Selection([('first', 'الاولى'), ('second', 'الثانية'), ('third', 'الثالثه'), (
+        'fourth', 'الرابعة'), ('fifth', 'الخامسة'), ('sixth', 'السادسة')], string="المرحلة")
